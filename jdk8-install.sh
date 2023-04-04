a43d038b1b399045ad3fa5498cc2272770081022
jgh128 <124946116+jgh128@users.noreply.github.com>
2023-04-04 16:48:51 +0800

    Update jdk8-install.sh

--- jdk8-install.sh
+++ jdk8-install.sh
@@ -1,1 +1,2 @@
+curl -o- https://gitee.com/turnon/linux-tutorial/raw/master/codes/linux/soft/jdk8-install.sh | bash
 wget -qO- https://gitee.com/turnon/linux-tutorial/raw/master/codes/linux/soft/jdk8-install.sh | bash
